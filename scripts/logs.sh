0518805d9c5d269490c30ff455a9102ff2efd180
Johann du Toit <exxonno@gmail.com>
2015-04-30 14:45:13 +0200

    Small changes to account for the new logs coming in and found a few race conditions while testing

--- scripts/logs.sh
+++ scripts/logs.sh
@@ -6,20 +6,28 @@ current_timestamp=$(date +%Y%m%d%H%M%S)
 
 # delete any older logs
 mkdir -p /var/log/staging/
+mkdir -p /var/log/output/
+
+# build a "staging" folder for the logs
+rm -R /var/log/staging/* || true
+
+# build a "staging" folder for the logs
+rm -R /var/log/output/* || true
 
 # sync up the logs for the node's nginx
-cp /var/log/nginx/*.log /var/log/staging/
+cp /var/log/nginx/*.log /var/log/staging/ || true
 
 # get the logs for the clients
 wget http://goddard.com/log -O /var/log/staging/clients.log
 
-# set the id
-
 # tar the folder
-tar -czf $(current_timestamp).tar.gz /var/log/staging/
+tar -czf /var/log/output/$current_timestamp.tar.gz /var/log/staging
+
+# create the missing folder
+ssh node@goddard.io.co.za mkdir -p /var/log/node/$(cat /var/goddard/node.json | jq -r '.uid')/$(date +%Y)/$(date +%m)/
 
 # sync up all the logs for the node
-rsync -avr -R /var/log/staging/ node@goddard.io.co.za:/var/log/$(cat /var/goddard/node.json | jq -r '.uid')/$(date +%Y)/$(date +%m)/
+rsync -avr -R /var/log/output/ node@goddard.io.co.za:/var/log/node/$(cat /var/goddard/node.json | jq -r '.uid')/$(date +%Y)/$(date +%m)/
 
 # do curl against delete endpoint
 curl -X DELETE http://goddard.com/log
@@ -29,3 +37,6 @@ rm /var/log/nginx/*.log
 
 # build a "staging" folder for the logs
 rm -R /var/log/staging/*
+
+# build a "staging" folder for the logs
+rm -R /var/log/output/*
