edc176a2b98709a3bf27dc81c3deb96e2cf79f80
Johann du Toit <exxonno@gmail.com>
2015-05-07 09:54:13 +0200

    writing out the NGINX variables as quoted rather

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -102,12 +102,12 @@ if [ $ret_code = 0 ]; then
 
 				location / {
 
-					proxy_pass                  http://127.0.0.1:${tport}$request_uri;
+					proxy_pass                  http://127.0.0.1:${tport}\$request_uri;
 					proxy_redirect              off;
 
-					proxy_set_header            Host             $host;
-					proxy_set_header            X-Real-IP        $remote_addr;
-					proxy_set_header            X-Forwarded-For  $proxy_add_x_forwarded_for;
+					proxy_set_header            Host             \$host;
+					proxy_set_header            X-Real-IP        \$remote_addr;
+					proxy_set_header            X-Forwarded-For  \$proxy_add_x_forwarded_for;
 					proxy_max_temp_file_size    0;
 
 					client_max_body_size        10m;
