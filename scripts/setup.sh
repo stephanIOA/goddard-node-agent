b681e001412e6a559f9212afa17cd9cd98fe8449
Johann du Toit <exxonno@gmail.com>
2015-09-14 16:59:44 +0200

    Updated with new fixes after going through the file with a few eye-balls, next up are tests

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,7 +7,7 @@ mkdir -p /var/goddard
 if [ ! -f /var/goddard/setup.lock ]; then
 
 	# write the lock
-	echo data > /var/goddard/setup.lock
+	echo date > /var/goddard/setup.lock
 
 	# done
 	echo "{\"build\":\"busy\",\"process\":\"Loading base image\",\"timestamp\":\"$( date +%s )\"}" > /var/goddard/build.json
@@ -24,17 +24,24 @@ if [ ! -f /var/goddard/setup.lock ]; then
 	# post to server
 	curl -X POST -d @/var/goddard/build.json http://hub.goddard.unicore.io/report.json?uid=$(cat /var/goddard/node.json | jq -r '.uid') --header "Content-Type:application/json"
 
-	# read in all the apps from the server
-	curl "http://hub.goddard.unicore.io/apps.json?uid=$(cat /var/goddard/node.json | jq -r '.uid')" > /var/goddard/apps.raw.json
-
 	# reload flag
 	nginx_reload_flag=0
+
+	# read in all the apps from the server
+	curl "http://hub.goddard.unicore.io/apps.json?uid=$(cat /var/goddard/node.json | jq -r '.uid')" > /var/goddard/apps.raw.json
 
 	# register the return code
 	ret_code=$?
 
 	# check the code, must be 0
 	if [ $ret_code = 0 ]; then
+
+		# check the diff first
+		DIFF=$(diff /var/goddard/apps.raw.json /var/goddard/apps.json) 
+		if [ "$DIFF" != "" ] 
+		then
+			nginx_reload_flag=1
+		fi
 
 		# move the json to live node details
 		mv /var/goddard/apps.raw.json /var/goddard/apps.json
@@ -64,8 +71,8 @@ if [ ! -f /var/goddard/setup.lock ]; then
 			# sync down the code
 			amount=$(rsync -aPzri --progress node@hub.goddard.unicore.io:/var/goddard/apps/$tkey/ /var/goddard/apps/$tkey | wc -l)
 
-			# check if the exit code was a 1, so 0 ...
-			if [ "$amount" -lt 1 ]; then
+			# check the amount changed files
+			if [ "$amount" -gt 0 ]; then
 
 				# mark as 'yes'
 				nginx_reload_flag=1
@@ -102,53 +109,6 @@ if [ ! -f /var/goddard/setup.lock ]; then
 
 				# start the app
 				cd /var/goddard/apps/$tkey && docker run --restart=always -p $tport:8080 -d $tkey
-
-				# done
-				echo "Adding $tdomain web server config"
-
-				# done
-				echo "{\"build\":\"busy\",\"process\":\"Adding $tdomain web server config\",\"timestamp\":\"$( date +%s )\"}"  > /var/goddard/build.json
-
-				# post to server
-				curl -X POST -d @/var/goddard/build.json http://hub.goddard.unicore.io/report.json?uid=$(cat /var/goddard/node.json | jq -r '.uid') --header "Content-Type:application/json"
-
-				# write out the service file
-				sudo cat <<-EOF > /etc/nginx/conf.d/$tdomain.conf
-
-					server {
-
-						listen                          80;
-						server_name                     $tdomain;
-						access_log                      /var/log/nginx/${tkey}.access.log;
-						error_log                       /var/log/nginx/${tkey}.error.log;
-
-						location / {
-
-							proxy_pass                  http://127.0.0.1:${tport}\$request_uri;
-							proxy_redirect              off;
-
-							proxy_set_header            Host             \$host;
-							proxy_set_header            X-Real-IP        \$remote_addr;
-							proxy_set_header            X-Forwarded-For  \$proxy_add_x_forwarded_for;
-							proxy_max_temp_file_size    0;
-
-							client_max_body_size        10m;
-							client_body_buffer_size     128k;
-
-							proxy_connect_timeout       120;
-							proxy_send_timeout          1200;
-							proxy_read_timeout          120;
-
-							proxy_buffer_size           128k;
-							proxy_buffers               4 256k;
-							proxy_busy_buffers_size     256k;
-							proxy_temp_file_write_size  256k;
-
-						}
-
-					}
-
-				EOF
 
 			fi
 
